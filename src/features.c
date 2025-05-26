0c52760cb8a3b02be224f3098dfe40c648aa0e9d
Xavierlecozic <xavier.lecozic@etu.estia.fr>
2025-05-26 15:20:06 +0200

    fonction tenth pixel

--- src/features.c
+++ src/features.c
@@ -55,3 +55,22 @@ void first_pixel (char *source_path){
     
 }
 
+
+void tenth_pixel (char *source_path){
+    int width;
+    int height;
+    int channel_count;
+    unsigned char *data;
+
+    int couleur = read_image_data(source_path, &data, &width, &height, &channel_count);
+    int R=data[27], G=data[28], B=data[29];
+
+    if (couleur){
+        printf("first_pixel: %d, %d, %d", R, G, B);
+    }
+    else {
+        printf("erreur:");
+    }
+    
+}
+
