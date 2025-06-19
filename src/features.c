31d34379203bfd7da7c7bec66107df42e847ccf4
Leo-Battaglia <leo.battaglia@etu.estia.fr>
2025-06-19 14:38:47 +0200

    debug : color_invert

--- src/features.c
+++ src/features.c
@@ -518,7 +518,7 @@ void color_invert(char* source_path) {
         }
     }
 
-    write_image_data("output_inverted.bmp", pixels, img_width, img_height);
+    write_image_data("image_out.bmp", pixels, img_width, img_height);
 }
 
     void color_desaturate(char* source_path) {
